592d71e8e4a7bf88679848dbcbdeadb927b776f7
nadeem <nadeem.eway@gmail.com>
2020-08-14 17:51:32 +0530

    li3backend

---
+++ app/views/my_posts/view.html.php
@@ -0,0 +1,4 @@
+<h1><?=$myPost->title ?></h1>
+<p><?=$myPost->body ?></p>
+<a href="/my_posts/">View All Posts</a>
+<p><a href="/my_posts/delete/<?=$myPost->id ?>/">Delete This Post</a></p>
---
+++ app/views/posts/view.html.php
@@ -0,0 +1,3 @@
+<h1><?=$myPost->title ?></h1>
+<p><?=$myPost->body ?></p>
+<a href="/my_posts/">View All Posts</a>
