c412ecedbbd4fa410f607ee59017f6ca611b9e8f
nadeem <nadeem.eway@gmail.com>
2020-08-17 18:52:16 +0530

    new api controller with services table modal

---
+++ app/models/Services.php
@@ -0,0 +1,13 @@
+<?php
+
+//Use the app\models namespace at the beginning of all model files
+namespace app\models;
+
+//All new models must extend the \lithium\data\Model class
+class Services extends \lithium\data\Model {
+
+    //The Services class is intentionally empty right now
+
+}
+
+?>
