5ea376387f92de6c70c4ee0745661979263ed992
nadeem <nadeem.eway@gmail.com>
2020-08-24 18:49:06 +0530

    add data api update

---
+++ app/models/Leads.php
@@ -0,0 +1,13 @@
+<?php
+
+//Use the app\models namespace at the beginning of all model files
+namespace app\models;
+
+//All new models must extend the \lithium\data\Model class
+class Leads extends \lithium\data\Model {
+
+    //The Leads class is intentionally empty right now
+
+}
+
+?>
